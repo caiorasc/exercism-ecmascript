5874fd4e36e3ebc90dd3244246a7bd5ce3d4f5b2
Caio Costa <caio@nfe.io>
2018-06-21 21:27:40 +0000

    Start resolve grade-school exercise

---
+++ grade-school/grade-school.js
@@ -0,0 +1,20 @@
+class School {
+    roster() {
+        return {};
+    }
+
+    add(name, grade) {
+        console.log(name, grade);
+    }
+
+    roster() {
+
+        return {};
+    }
+
+
+}
+
+
+
+export default School;
