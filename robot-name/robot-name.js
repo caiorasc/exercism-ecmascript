dd93e706d0622a128f256d6fcb1621d5864bea67
Caio Costa <caio@nfe.io>
2018-07-16 22:41:58 +0000

    old exercise

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -2,18 +2,28 @@ class Robot {
     constructor(){
         this.letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
         this.name = this.robotName();
+        this.count = new Set();
     }
 
     robotName() {
         let lettersIndex = this.randomLetter();
         let robotName = this.letters[lettersIndex[0]] + this.letters[lettersIndex[1]];
+        let x = robotName + this.randomNumber() 
 
-        return robotName + this.randomNumber();
+        return x;
     }
 
     reset() {
         this.name = this.robotName();
-        
+        let size = this.count.size;
+
+        this.count.add(this.name);
+
+        while (this.count.size == size) {
+            this.name = this.robotName();
+            this.count.add(this.name);
+        }
+
         return this.name
     }
 
