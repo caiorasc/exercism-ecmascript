cc3339a1677a54937cb72d1995d6af634b11480c
Caio Costa <caio@nfe.io>
2018-07-17 23:21:42 +0000

    Finish two-fer exercise

---
+++ two-fer/two-fer.js
@@ -0,0 +1,5 @@
+export default function twoFer(name) {
+    let resultStr = (name == '') ? resultStr = `One for you, one for me.` : resultStr = `One for ${name}, one for me.`;
+
+    return resultStr;
+}
