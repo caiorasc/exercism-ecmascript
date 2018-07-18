4b9e2aef4b0d893bec87b09a311a644591e59dff
Caio Costa <caio@nfe.io>
2018-07-18 22:33:15 +0000

    finish isogram exercise

--- isogram/isogram.js
+++ isogram/isogram.js
@@ -1,5 +1,24 @@
 class Isogram {
-    
+    constructor(word) {
+        this.word = word.toLowerCase();
+        this.uniqueList = new Set();
+    }
+
+    isIsogram() {
+        let onlyLetters = [];
+
+        for (let i = 0; i < this.word.length; i++) {
+            if (this.word[i].match(/(?![×÷])[A-Za-zÀ-ÿ]/)) {
+                onlyLetters.push(this.word[i]);
+                this.uniqueList.add(this.word[i]);
+            }
+        }
+
+        if (this.uniqueList.size == onlyLetters.length)
+            return true
+
+        return false;
+    }
 }
 
 export default Isogram;
