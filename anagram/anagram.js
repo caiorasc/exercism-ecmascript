68ed79fccdf61e9abcfc531755bd0548ed6d49f8
Caio Costa <caio@nfe.io>
2018-07-19 00:32:16 +0000

    Eight tests passing, one fail

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -1,11 +1,24 @@
 class Anagram {
     constructor(word) {
-        this.word = word;
-        console.log(this.word.split('').sort());
+        this.word = word.toLowerCase().split('').sort();
     }
 
     matches(matchArray) {
-        console.log(matchArray);
+        let matchArrayCopy = matchArray.slice();
+        let result = [];
+
+        for (let i = 0; i < matchArrayCopy.length; i++) {
+            let element = matchArrayCopy[i].toLowerCase().split('').sort();
+
+            let strElement = element.join('')
+            let ordenateWord = this.word.join('');
+
+            if (strElement == ordenateWord) {
+                result.push(matchArray[i]);
+            }
+        }
+
+        return result;
     }
 }
 
