08102327538dfde0b919fb6465d81b69c631e91c
Caio Costa <caio@nfe.io>
2018-08-12 14:55:09 +0000

    Finish word-count exercise

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,10 +1,10 @@
 class Words {
-
     count(phrase) {
+        let str = phrase.replace(/(\r\n|\n|\t|\r|\s{2,})/gm, ' ').trim();
         let result = {};
-        let phraseArr = phrase.toLowerCase().split(' ').sort();
+        let phraseArr = str.toLowerCase().split(' ').sort();
         let count = 1;
-
+        
         for (let i = 0; i < phraseArr.length; i++) {
             if (phraseArr[i] == phraseArr[i - 1]) {
                 count++
