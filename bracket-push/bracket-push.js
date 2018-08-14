bbdd1e9f33d10bd913609c8c4a1cb5e801de083b
Caio Costa <caio@nfe.io>
2018-08-14 23:28:27 +0000

    Finish bracket-push exercise

--- bracket-push/bracket-push.js
+++ bracket-push/bracket-push.js
@@ -1,6 +1,45 @@
 export default function bracket(str) {
-    console.log(str);
-    if (str == '{}') {
+    let matchClose = [')', ']', '}'];
+    let matchOpen = ['{', '[', '('];
+    let stack = [];
+    let controlClosedBrackets = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let openBracket = '';
+
+        if (matchOpen.includes(str[i])) {
+            stack.push(str[i]);
+        }
+
+        if (matchClose.includes(str[i])) {
+            openBracket = getOpenBracket(str[i]);
+            controlClosedBrackets.push(str[i]);
+        }
+
+        if (openBracket == stack[stack.length - 1]) {
+            stack.pop();
+            controlClosedBrackets.pop();
+        }
+    }
+
+    if (stack.length == 0 && controlClosedBrackets.length == 0)
         return true;
+
+    return false;
+}
+
+function getOpenBracket(closedBracket) {
+    switch (closedBracket) {
+        case '}':
+            return '{';
+            break;
+
+        case ')':
+            return '(';
+            break;
+
+        case ']':
+            return '[';
+            break;
     }
 }
