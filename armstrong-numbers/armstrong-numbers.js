c36d9c52a42fd124f0ebea0bf31ba603f525f3b8
Caio Costa <caio@nfe.io>
2018-08-22 23:55:38 +0000

    Finish armstrong-numbers exercise

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -1,3 +1,23 @@
 export function validate(input) {
-    return null;
+    let strInput = input.toString();
+    let toCalcInput = input;
+    let result = 0;
+
+    if (strInput.length == 2)
+        return false;
+
+    for (let i = 0; i < strInput.length; i++) {
+        let digit = Math.trunc(toCalcInput) % 10;
+        toCalcInput = toCalcInput / 10;
+
+        if (toCalcInput > 0 && i == 0) {
+            result += Math.pow(digit, strInput.length);
+
+        } else {
+            let x = Math.pow(digit, strInput.length)
+            result += x;
+        }
+    }
+
+    return (result == input) ? true : false;
 }
