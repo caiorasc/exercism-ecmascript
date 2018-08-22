36b76178bb1c27663cf877bec81ee8c479dfb78e
Caio Costa <caio@nfe.io>
2018-08-22 22:17:22 +0000

    Finish series exercise

--- series/series.js
+++ series/series.js
@@ -11,6 +11,25 @@ class Series {
 
         return digits;
     }
+
+    slices(slicesNumber) {
+        let digitsCopy = this.digits.slice(0, this.digits.length);
+        let slicedArray = [];
+
+        if (digitsCopy.length < slicesNumber) {
+            throw new Error('Slice size is too big.');
+        }
+
+        for (let i = 0; i < this.digits.length; i++) {
+            if (digitsCopy.length >= slicesNumber) {
+                slicedArray.push(digitsCopy.slice(0, slicesNumber))
+                digitsCopy.shift();
+
+            }
+        }
+
+        return slicedArray;
+    }
 }
 
 export default Series;
