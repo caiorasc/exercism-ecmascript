b311c1db620c8871c3d2b0e7b6e839e8f4b00a38
Caio Costa <caio@nfe.io>
2018-08-23 16:29:57 +0000

    Finish perfect-numbers exercise

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -1,6 +1,23 @@
 class PerfectNumbers {
     classify(number) {
-        return 'perfect';
+        let sum = 0;
+
+        if (number <= 0)
+            throw 'Classification is only possible for natural numbers.'
+
+        for (let i = 0; i < number; i++) {
+            let resultDivision = number % i;
+            if (resultDivision == 0)
+                sum += i;
+        }
+
+        if (sum == number)
+            return 'perfect';
+
+        if (sum > number)
+            return 'abundant';
+
+        return 'deficient';
     }
 }
 
