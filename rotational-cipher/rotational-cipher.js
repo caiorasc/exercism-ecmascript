130058e72f0409f9829d976e3c85400f4cff8562
Caio Costa <caio@nfe.io>
2018-08-31 00:30:04 +0000

    Finish rotational-cipher exercise

--- rotational-cipher/rotational-cipher.js
+++ rotational-cipher/rotational-cipher.js
@@ -1,7 +1,32 @@
 class RotationalCipher {
     static rotate(str, key) {
-        
-        return null;
+        let alphabet = ('abcdefghijklmnopqrstuvwxyz').split('');
+        let result = [];
+
+
+        for (let i = 0; i < str.length; i++) {
+            let position = alphabet.indexOf(str[i].toLowerCase());
+            let positionControl = position + key;
+
+            if (position >= 0 && positionControl < 26) {
+                position += key;
+                (str[i].toLowerCase() === str[i]) ?
+                    result.push(alphabet[position]) :
+                    result.push(alphabet[position].toUpperCase());
+
+            } else {
+                if (positionControl >= 26) {
+                    (str[i].toLowerCase() === str[i]) ?
+                        result.push(alphabet[positionControl - 26]) :
+                        result.push(alphabet[positionControl - 26].toUpperCase());
+                } else {
+                    result.push(str[i]);
+                }
+
+            }
+        }
+
+        return result.join('');
     }
 }
 
