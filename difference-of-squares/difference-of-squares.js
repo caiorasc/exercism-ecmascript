dad7f35159f2c6baf50b082319d58d76e09a9de4
Caio Costa <caio@nfe.io>
2018-08-31 22:04:36 +0000

    Finish difference-of-squares exercise

--- difference-of-squares/difference-of-squares.js
+++ difference-of-squares/difference-of-squares.js
@@ -1,6 +1,31 @@
 class Squares {
     constructor(number) {
         this.number = number;
+        this.squareOfSums = this.squareOfSums();
+        this.sumOfSquares = this.sumsOfsquare();
+        this.difference = this.squareOfSums - this.sumOfSquares;
+    }
+
+    squareOfSums() {
+        let number = this.number;
+        let sum = 0;
+
+        for (let i = 1; i <= number; i++) {
+            sum += i;
+        }
+
+        return Math.pow(sum, 2);
+    }
+
+    sumsOfsquare() {
+        let number = this.number;
+        let sum = 0;
+
+        for (let i = 1; i <= number; i++) {
+            sum += Math.pow(i, 2);
+        }
+
+        return sum;
     }
 }
 
