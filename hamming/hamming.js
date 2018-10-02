ed8e0bf5f69eb7c525d08f97c892c518e42a3b51
Caio Costa <caio@nfe.io>
2018-10-02 00:54:27 +0000

    Finis hamming exercise

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,6 +1,19 @@
 class Hamming {
     compute(dnaStand1, dnaStand2) {
-        console.log(dnaStand1, dnaStand2);
+        let count = 0;
+
+        if (dnaStand1.length != dnaStand2.length) {
+            throw new Error('DNA strands must be of equal length.');
+        }
+
+        for (let i = 0; i < dnaStand1.length; i++) {
+            if (dnaStand1[i] != dnaStand2[i]) {
+                count++;
+            }
+
+        }
+
+        return count;
     }
 }
 
