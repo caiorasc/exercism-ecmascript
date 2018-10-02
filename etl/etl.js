9be347fb998cb36c0be2b09c8371cc9a55a45a38
Caio Costa <caio@nfe.io>
2018-10-02 00:17:01 +0000

    Finish etl exercise

--- etl/etl.js
+++ etl/etl.js
@@ -1,5 +1,14 @@
 export default function transform(scores) {
     let newScores = {};
+    let oldArrayValues = Object.values(scores);
+    let oldArrayKeys = Object.keys(scores);
+
+    for (let i = 0; i < oldArrayValues.length; i++) {
+        for (let j = 0; j < oldArrayValues[i].length; j++) {
+            newScores[oldArrayValues[i][j].toLowerCase()] = parseInt(oldArrayKeys[i]);
+        }
+
+    }
 
     return newScores;
 }
