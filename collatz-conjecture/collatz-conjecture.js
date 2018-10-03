3fd3d9026cfbe5428d04c20264cfd8067e3c9d90
Caio Costa <caio@nfe.io>
2018-10-03 11:16:53 +0000

    Finish collatz-conjucture exercise

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,3 +1,16 @@
 export const steps = (number) => {
-    console.log(number);
+    let result = number;
+    let steps = 0;
+
+    if (result < 1) {
+        throw new Error('Only positive numbers are allowed');
+    }
+
+    while (result > 1) {
+        result = (result % 2 == 0) ? result / 2 : 3 * result + 1;
+        steps++;
+
+    }
+
+    return steps;
 }
